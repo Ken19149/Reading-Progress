2ecdc875d8940a7da786f90e3f60d1c73e524465
Ken980 <kenboy9800@gmail.com>
2025-04-05 16:12:35 +0700

    add page count and speed to timer

--- data.py
+++ data.py
@@ -3,4 +3,4 @@ import io
 path = open("path", "r").read()
 print(path)
 data = io.open(path, "r", encoding="utf-8").read()
-
+print(data)
--- timer.py
+++ timer.py
@@ -1,24 +1,47 @@
 import datetime
 import pyperclip
+import io
+
+# timer
+
+print("\nReading Progress Timer Activated\n")
 
 today = str(datetime.date.today()).split(" ")[0].replace("-","")
 
-input("press enter to start: ")
+pageBegin = (input("input start page: "))
+print(f"page begin: {pageBegin}")
 start = datetime.datetime.now()
 startTime = str(start).split(" ")[1].split(".")[0].replace(":","")[0:4]
 print(f"start time: {start}\n")
 
-input("press enter again to pause: ")
+pageEnd = (input("input end page when finish reading: "))
 end = datetime.datetime.now()
 endTime = str(end).split(" ")[1].split(".")[0].replace(":","")[0:4]
 print(f"end time: {end}\n")
 
-dif = (end - start)
-difMin = (int(str(dif).split(":")[0]) * 60) + int(str(dif).split(":")[1])
-difSec = float(str(dif).split(":")[2])
-if difSec >= 30: difMin+=1
+pageOffset = (input("type page offset (default=0): "))
+pageCount = int(pageEnd) - int(pageBegin) + int(pageOffset)
+print(f"page count: {pageCount}")
 
+dif = (end - start)
+difHour = int(str(dif).split(":")[0])
+difMin = int(str(dif).split(":")[1])
+difSec = float(str(dif).split(":")[2])
+difFormat = (difHour * 60) + difMin
+if difSec >= 30: difFormat+=1
 print(f"dif: {dif}\n")
-format = f"{difMin} {today} {startTime} {endTime}"
+
+speed_ppm = pageCount/(((difHour*60*60) + (difMin*60) + difSec)/60) # pages per min
+speed_mpp = 1/speed_ppm # mins per page
+speed_spp = speed_mpp*60 # seconds per page
+print(f"speed:\n{speed_ppm} pages/min\n{speed_mpp} mins/page\n{speed_spp} seconds/page\n")
+
+format = f"{pageEnd} {pageCount} {difFormat} {today} {startTime} {endTime}"
 pyperclip.copy(format)
 print(f"Format: {format} \nCopied to Clipboard")
+
+# stats updater
+
+path = open("path", "r").read()
+print(path)
+data = io.open(path, "r", encoding="utf-8").read()
